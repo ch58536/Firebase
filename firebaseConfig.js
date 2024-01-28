7fd09f7bead4d596fbf7aeafb4db666c9d4f15de
ch58536 <116449925+ch58536@users.noreply.github.com>
2024-01-28 11:58:48 +0100

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+// Import Firebase
+import firebase from 'firebase';
+
+// Zainicjalizuj Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBw1LGlxilWzXjcfrObDQGJHJ5gS5c6KoE",
+  authDomain: 'zaliczenie58536.firebaseapp.com',
+  databaseURL: 'https://zaliczenie58536.firebaseio.com',
+  projectId: 'zaliczenie58536',
+  storageBucket: 'zaliczenie58536.appspot.com',
+  messagingSenderId: 'SENDER_ID',
+  appId: 'APP_ID',
+};
+
+firebase.initializeApp(firebaseConfig);
